38e7aac5b1ab74dbb87c864c7fbd74a2cdcbe658
yehan_zhou <yehan_zhou@kingdee.com>
2020-01-17 17:36:05 +0800

    add： vue source

---
+++ vue-source/easy-vue.js
@@ -0,0 +1,57 @@
+class Vue {
+    constructor (opt) {
+        this.opt = opt
+        this.observe(opt.data)
+        const el = document.querySelector(opt.el)
+        this.compile(el)
+    }
+
+    observe (data) {
+        Object.keys(data).forEach(key => {
+            const val = data[key]
+            const dep = New Dep()
+            Object.defineProperty(data, key, {
+                get () {
+                    if (Dep.target) {
+                        dep.addSub(Dep.target) // 添加订阅者
+                    }
+                    return val
+                }
+                set (newVal) {
+                    val = newVal
+                    Dep.notify(newVal)
+                }
+            })
+        })
+    }
+
+    compile (node) {
+        [].forEach.call(node, child => {
+            if (!child.firstElementChild && /\{\{(.*)\}\}/.test(child.innerHTML)) {
+                let key = RegExp.$1.trim()
+                child.innerHTML = child.innerHTML.replace(new RegExp('\\{\\{\\s*'+ key +'\\s*\\}\\}', 'gm'),this.opt.data[key])
+                Dep.target = child
+                this.opt.data[key]
+                Dep.target = null
+            } else if (child.firstElementChild) {
+                this.compile(child)
+            }
+        })
+    }
+}
+
+class Dep { // 依赖收集器（发布者的工具）
+    constructor () {
+        this.subs = new Set()
+    }
+
+    addSub (target) {
+        this.subs.add(target)
+    }
+
+    notify (newVal) {
+        this.subs.forEach(sub => {
+            sub.innerHTML = newVal
+        })
+    }
+}
